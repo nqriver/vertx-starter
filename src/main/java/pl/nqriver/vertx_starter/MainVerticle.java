6185787741d22f80da9ae9dc5716cd6eb691554e
nqriver <nqriver@gmail.com>
2022-08-10 12:44:55 +0200

    Multiple verticles

--- src/main/java/pl/nqriver/vertx_starter/MainVerticle.java
+++ src/main/java/pl/nqriver/vertx_starter/MainVerticle.java
@@ -1,10 +1,15 @@
 package pl.nqriver.vertx_starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import pl.nqriver.vertx_starter.verticles.VerticleA;
 import pl.nqriver.vertx_starter.verticles.VerticleB;
+import pl.nqriver.vertx_starter.verticles.VerticleN;
+
+import java.util.UUID;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -19,6 +24,14 @@ public class MainVerticle extends AbstractVerticle {
     System.out.println("Start " + getClass().getName());
     vertx.deployVerticle(new VerticleA());
     vertx.deployVerticle(new VerticleB());
+    vertx.deployVerticle(VerticleN.class.getName(),
+      new DeploymentOptions().setInstances(4)
+        .setConfig(
+          new JsonObject()
+            .put("id", UUID.randomUUID().toString())
+            .put("name", VerticleN.class.getSimpleName())
+        ));
+
     startPromise.complete();
   }
 }
